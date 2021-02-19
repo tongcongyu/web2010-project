fa8172b871b9799d2a449de2a7e1bfedb97b37a0
xiaonan <xiaonan@qq.com>
2021-02-19 18:51:32 +0800

    更新购物车未登录静态

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,22 +1,54 @@
 <template>
   <div class="cart">
-    <van-nav-bar title="购物车" left-text left-arrow @click-left="onClickLeft" />
-    <van-empty description="描述文字" />
-
-    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-      <!-- <van-cell v-for="item in list" :key="item" :title="item"> -->
-      <van-cell v-for="item in list" :key="item" :title="item">
-        <van-row type="flex" justify="space-between">
-          <van-col span="9">
-            <van-image width="100" height="100" src="https://img01.yzcdn.cn/vant/cat.jpeg" />
-          </van-col>
-          <van-col span="9">
-            <van-image width="100" height="100" src="https://img01.yzcdn.cn/vant/cat.jpeg" />
-          </van-col>
-        </van-row>
-      </van-cell>
-    </van-list>
-
+    <!-- 1.没有登录时的购物车 -->
+    <div class="noLogin">
+      <van-nav-bar title="购物车">
+        <template #left>
+          <router-link to="/"><van-icon name="arrow-left" size="0.5rem" /></router-link>
+        </template>
+      </van-nav-bar>
+      <div class="login-tip">
+        <span>登录后将同步您的购物车商品</span>
+        <van-button round type="info">登录</van-button>
+      </div>
+      <!-- <div class="login-tip"></div>
+      <van-notice-bar
+        color="#232628"
+        background="#fff0ec"
+        text="登录后将同步您的购物车商品" 
+      />
+      <van-button round type="info">登录</van-button> -->
+      <van-empty description="购物车内暂时没有商品" />
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+      >
+        <!-- <van-cell v-for="item in list" :key="item" :title="item"> -->
+        <van-cell v-for="item in list" :key="item" :title="item">
+          <van-row type="flex" justify="space-between">
+            <van-col span="9">
+              <van-image
+                width="100"
+                height="100"
+                src="https://img01.yzcdn.cn/vant/cat.jpeg"
+              />
+            </van-col>
+            <van-col span="9">
+              <van-image
+                width="100"
+                height="100"
+                src="https://img01.yzcdn.cn/vant/cat.jpeg"
+              />
+            </van-col>
+          </van-row>
+        </van-cell>
+      </van-list>
+    </div>
+    <!-- 2.登录时的购物车 -->
+    <div class="login"></div>
+    <!-- 3.底部导航栏 -->
     <total-tabbar></total-tabbar>
   </div>
 </template>
@@ -29,7 +61,7 @@ export default {
     return {
       list: [],
       loading: false,
-      finished: false
+      finished: false,
     };
   },
   methods: {
@@ -53,10 +85,13 @@ export default {
           this.finished = true;
         }
       }, 1000);
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
+.login-tip>span{
+
+}
 </style>
