e1330c34dff9364ce7d6f165c3ba6da4c7b8dee5
tongcongyu <1545333284@qq.com>
2021-02-24 08:45:30 +0800

    修改详情页3

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -14,40 +14,41 @@
         </van-nav-bar>
         <!-- 图片预览 -->
         <van-swipe @change="changeNum">
-            <van-swipe-item v-for="(value,indexaa) of 4" :key="indexaa"><van-image :src="image[indexaa]" width="7.5rem" height="8.18rem"></van-image></van-swipe-item>
+            <van-swipe-item v-for="(value,index01) of image" :key="index01"><van-image :src="image[index01]" width="7.5rem" height="8.18rem"></van-image></van-swipe-item>
             <template #indicator>
                 <div class="change_num">{{ current+1}}/4</div>
             </template>
         </van-swipe>
         <!-- 标题 -->
         <div class="title">
-            <div class="title01">{{details[0].title}}/{{details[0].title01}}<span class="title02">{{details[0].title02}}</span></div>
+            <div class="title01">{{result[2]}}<span class="title02">{{result[3]}}</span></div>
             <div class="star">
                 <van-icon name="star-o" size="0.4rem" />
             </div>
-            <div class="onprice"><span>￥{{details[0].price}}</span>以售{{details[0].sales_volume}}件</div>
+            <div class="onprice"><span>￥{{result[4]}}</span>以售{{result[5]}}件</div>
         </div>
         <!-- 选项 -->
         <div class="option">
-            <div class="option_item" v-for="(value,index09) of 4" :key="index09">
-                <p>{{option_title[index09]}}</p><p>{{option[index09]}}</p>
+            <div class="option_item" v-for="(value,index02) of 4" :key="index02">
+                <p>{{option[index02]}}</p><p>{{result[index02+6]}}</p>
             </div>
-            <!-- 类型 -->
-        
+            <!-- 商品规格 -->
+            <div class="option_item">
+                <p @click="sku_show">已选··</p><p @click="sku_show">11111</p>
+            </div>
+            <van-sku v-model="show" :sku="sku" :goods="goods" goods-id="1" @buy-clicked="onBuyClicked" @add-cart="onAddCartClicked"/>
         </div>
         <!-- 订单评论 -->
         <div class="comment">
             <div class="comment_top">订单评价<span>更多</span></div>
             <div>
-                <div class="comment_item" v-for="(value,indexqq) of 3" :key="indexqq">
-                    <van-image :src="details[indexqq].user_pic" width="20" class="user_pic"></van-image>
-                    <span>{{details[indexqq].nickname}}</span><van-rate v-model="star" disabled size="10" disabled-color='#ffd21e' />
-                    <p>{{details[indexqq].content}}</p>
+                <div class="comment_item" v-for="(value,index03) of 3" :key="index03">
+                    <van-image src="" width="20" class="user_pic"></van-image>
+                    <span></span><van-rate v-model="star" disabled size="10" disabled-color='#ffd21e' />
+                    <p></p>
                     <div class="comment_img">
-                        <van-image :src="details[indexqq].com_image01" width="80"></van-image>
-                        <van-image :src="details[indexqq].com_image02" width="80"></van-image>
-                        <van-image :src="details[indexqq].com_image03" width="80"></van-image>
-                        <van-image :src="details[indexqq].com_image04" width="80"></van-image>
+                        <van-image src="" width="80"></van-image>
+                        <van-image src="" width="80"></van-image>
                     </div>
                 </div>
                 <van-button type="primary" size="large" color="" to="">更多评论</van-button>
@@ -55,11 +56,7 @@
             <!-- 图文详情 -->
             <div class="text_pic">
                 <p>图文详情</p>
-                <van-image :src="details[0].image_text01"></van-image>
-                <van-image :src="details[0].image_text02"></van-image>
-                <van-image :src="details[0].image_text03"></van-image>
-                <van-image :src="details[0].image_text04"></van-image>
-                <van-image :src="details[0].image_text05"></van-image>
+                <van-image :src="result[index04+10]" v-for="(value,index04) of 5" :key="index04"></van-image>
             </div>
         </div>
         <!-- 底部商品导航栏 -->
@@ -75,13 +72,59 @@
 export default {
     data() {
         return {
-            star:3,
-            details:[],
-            current: 0,
             image:[],
-            option_title:['花语','材料','包装','配送'],
-            option:[],
-            comment_img:[]
+            result:[],
+            results:[],
+            resulted:[],
+            option:['花语','材料','包装','配送'],
+            current:0,
+            star:5,
+            // 商品规格
+            show:false,
+            sku: {
+                    // 所有sku规格类目与其值的从属关系，比如商品有颜色和尺码两大类规格，颜色下面又有红色和蓝色两个规格值。
+                    // 可以理解为一个商品可以有多个规格类目，一个规格类目下可以有多个规格值。
+                    tree: [
+                    {
+                        k_s: "s1", // skuKeyStr：sku 组合列表（下方 list）中当前类目对应的 key 值，value 值会是从属于当前类目的一个规格值 id
+                        v: [
+                        {
+                            id: "1", // skuValueId：规格值 id
+                            name: "红色", // skuValueName：规格值名称
+                            imgUrl: "https://b.yzcdn.cn/vant/sku/shoes-3.png", // 规格类目图片，只有第一个规格类目可以定义图片
+                        },
+                        {
+                            id: "2", // skuValueId：规格值 id
+                            name: "蓝色色", // skuValueName：规格值名称
+                            imgUrl: "https://b.yzcdn.cn/vant/sku/shoes-3.png", // 规格类目图片，只有第一个规格类目可以定义图片
+                        },
+                        ],
+                    },
+                    ],
+                    // 所有 sku 的组合列表，比如红色、M 码为一个 sku 组合，红色、S 码为另一个组合
+                    list: [
+                    {
+                        id: 1, // skuId
+                        s1: "1", // 规格类目 k_s 为 s1 的对应规格值 id
+                        price: 200, // 价格（单位分）
+                        stock_num: 10000, // 当前 sku 组合对应的库存
+                    },
+                    {
+                        id: 2, // skuId
+                        s1: "2", // 规格类目 k_s 为 s1 的对应规格值 id
+                        price: 5990, // 价格（单位分）
+                        stock_num: 10000, // 当前 sku 组合对应的库存
+                    },
+                    ],
+                    price: "1.00", // 默认价格（单位元）
+                    stock_num: 99999, // 商品总库存
+                    collection_id: 2261, // 无规格商品 skuId 取 collection_id，否则取所选 sku 组合对应的 id
+                    none_sku: false, // 是否无规格商品
+                    hide_stock: false, // 是否隐藏剩余库存
+            },
+            goods: {
+                
+            },
         }
     },
     methods:{
@@ -93,6 +136,16 @@ export default {
         changeNum(index) {
             this.current = index;
         },
+        // 启动商品规格
+        sku_show(){
+            this.show=true;
+        },
+        onBuyClicked(){
+            console.log(1)
+        },
+        onAddCartClicked(){
+            console.log(2);
+        }
     },
     mounted() {
         // 获取路由id参数
@@ -103,33 +156,14 @@ export default {
                 id:id
             }
         }).then(res=>{
-            this.details=res.data
-            console.log(this.details);
-            // 将商品图片添加到image
-            this.image.push(this.details[0].image01) 
-            this.image.push(this.details[0].image02) 
-            this.image.push(this.details[0].image03) 
-            this.image.push(this.details[0].image04)
-            // 获取option
-            this.option.push(this.details[0].flower_means)
-            this.option.push(this.details[0].material)
-            this.option.push(this.details[0].package)
-            this.option.push(this.details[0].range)
-            // 获取评论图片
-            if(this.details)
-            this.comment_img.push(this.details[0].com_image01)
-            this.comment_img.push(this.details[0].com_image02)
-            this.comment_img.push(this.details[0].com_image03)
-            this.comment_img.push(this.details[0].com_image04)
-            for(var i=0;i<this.comment_img.length;i++){
-                if(this.comment_img[i]==''){
-                    this.comment_img.splice(i,1);
-                    i--
-                }
-            }
-            console.log(this.comment_img);
+            console.log(res.data)
+            // 把对象转为数组
+            this.image=Object.values(res.data.image)
+            this.result=Object.values(res.data.result);
+            this.results=Object.values(res.data.results);
+            this.resulted=res.data.resulted;
+        
         })
-        // 获取评论数
     },
 }
 </script>
@@ -208,10 +242,11 @@ export default {
     width: 94%;
 }
 .comment_top{
-    height: 0.6rem;
+    height: 0.8rem;
+    line-height: 0.8rem;
     font-size: 0.32rem;
     padding: 0 0.2rem;
-    border-bottom: 5px #F3F5F7 solid;
+    border-bottom: 3px #F3F5F7 solid;
 }
 .comment_top span{
     font-size: 0.24rem;
@@ -231,7 +266,9 @@ export default {
 }
 .comment_img{
     display: flex;
+    width: 50%;
     margin-bottom: 0.4rem;
+    justify-content: space-between;
 }
 /* 图文详情 */
 .text_pic{
